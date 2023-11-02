cfbae20398113bc359a22bcad9db638f121a0a56
Moi1oM <hcloud0806@gmail.com>
2023-11-02 21:23:01 +0900

    feat: inMemory Array for posts

--- server/main.js
+++ server/main.js
@@ -13,6 +13,9 @@ app.use(cors());
 // Body-parser middleware 사용 설정
 app.use(bodyParser.json());
 
+// 인메모리 데이터베이스 역할을 할 배열
+const posts = [];
+
 // SQLite 데이터베이스 연결
 const db = new sqlite3.Database("sqlite3.db", (err) => {
   if (err) {
@@ -29,7 +32,7 @@ const db = new sqlite3.Database("sqlite3.db", (err) => {
 app.post("/posts", (req, res) => {
   const { title, content, img_src } = req.body;
 
-  if (!title || !content) {
+  if (!title || !content || !img_src) {
     return res.status(400).json({ error: "Title and content are required." });
   }
 
@@ -39,12 +42,20 @@ app.post("/posts", (req, res) => {
       console.error(err.message);
       res.status(500).json({ error: err.message });
     } else {
-      res.status(201).json({ id: this.lastID, title, content, img_src });
+      const newPost = { id: this.lastID, title, content, img_src };
+      posts.push(newPost);
+      res.status(201).json(newPost);
     }
   });
 });
 
 app.get("/posts/all", (req, res) => {
+  // 캐시된 데이터가 있다면 바로 반환
+  if (posts.length > 0) {
+    return res.status(200).json(posts);
+  }
+
+  // 캐시된 데이터가 없다면 DB에서 불러온 후 캐시
   const query = "SELECT * FROM posts";
   db.all(query, [], (err, rows) => {
     if (err) {
@@ -52,6 +63,8 @@ app.get("/posts/all", (req, res) => {
       res.status(500).json({ error: err.message });
     } else {
       rows.reverse();
+      // DB에서 불러온 데이터를 캐시에 저장
+      posts.push(...rows);
       res.status(200).json(rows);
     }
   });
@@ -87,6 +100,9 @@ app.delete("/posts/:id", (req, res) => {
     if (this.changes === 0) {
       res.status(404).json({ error: "Post not found" });
     } else {
+      // 배열에서 삭제된 포스트 제거
+      const index = posts.findIndex((post) => post.id == id);
+      if (index !== -1) posts.splice(index, 1);
       res.status(200).json({ message: "Post deleted successfully" });
     }
   });
